2ee533fbb7784ed51cb179996f9916effbf87b2c
SushantP199 <sushantpagam199@gmail.com>
2022-08-11 18:31:38 +0530

    Q5 Year Is Leap Year Or Not Updated

--- 03 Conditional Statements/Q5 Year Is Leap Year Or Not/YearIsLeapYearOrNot.java
+++ 03 Conditional Statements/Q5 Year Is Leap Year Or Not/YearIsLeapYearOrNot.java
@@ -16,10 +16,22 @@ public class YearIsLeapYearOrNot {
         short year = scanner.nextShort();
         scanner.close();
 
+        /*
+        
         boolean isLeapYearCondition1 = year % 4 == 0 && year % 100 != 0;
         boolean isLeapYearCondition2 = year % 4 == 0 && year % 100 == 0 && year % 400 == 0;
 
         if (isLeapYearCondition1 || isLeapYearCondition2) {
+            System.out.println(year + " is a leap year");
+        } else {
+            System.out.println(year + " is not a leap year");
+        }
+        
+        */
+
+        // Simplified logic based time line analysis o notes
+
+        if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
             System.out.println(year + " is a leap year");
         } else {
             System.out.println(year + " is not a leap year");
